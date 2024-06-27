a1cc34a8ec63b03a418c29855bfaf63ea98fa370
bekhzod-olimov <bekhzod.olimov@gmail.com>
2024-06-27 16:51:20 +0900

    profile pic filetype bug fix

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,5 +1,5 @@
 import { Container } from "./styles";
-import bekhzod from "../../assets/Bekhzod.jpg";
+import bekhzod from "../../assets/Bekhzod.JPG";
 import pytorch from "../../assets/pytorch.svg";
 import tensorflow from "../../assets/tensorflow.svg";
 import python from "../../assets/python.svg";
