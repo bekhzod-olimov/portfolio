1e83dec37bf5efc6c5522daab6e25f37e30bcbed
bekhzod-olimov <bekhzod.olimov@gmail.com>
2024-06-27 16:26:00 +0900

    project error update

--- src/components/Project/Project.tsx
+++ src/components/Project/Project.tsx
@@ -16,7 +16,7 @@ export function Project() {
           <div className="project">
             <header>
               <svg width="50" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="#23ce6b" strokeWidth="1" strokeLinecap="round" strokeLinejoin="round"> <title>Folder</title> <path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path> </svg>
-              <div className="paper-links">
+              <div className="project-links">
               <a href="https://link.springer.com/article/10.1007/s00530-020-00726-w" target="_blank" rel="noreferrer">
                   <img src={scholarIcon} alt="Visit site" /></a>
                 <a href="https://link.springer.com/article/10.1007/s00530-020-00726-w" target="_blank" rel="noreferrer">
