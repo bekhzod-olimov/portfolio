d259110d900a66a4dd309151b77f6830be0de259
bekhzod-olimov <bekhzod.olimov@gmail.com>
2024-06-28 16:34:30 +0900

    change width

--- src/components/About/styles.ts
+++ src/components/About/styles.ts
@@ -60,7 +60,7 @@ export const Container = styled.section`
    }
   }
 
-  @media only screen and (max-width: 120px) {
+  @media only screen and (max-width: 480px) {
     .about-image {
       // max-width: 100%;
       max-width: 20%;
@@ -77,7 +77,7 @@ export const Container = styled.section`
     }
   }
 
-  @media (max-width: 120px){
+  @media (max-width: 480px){
     display: block;
     text-align: center;
     
