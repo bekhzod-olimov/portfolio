e55e2284a3245b91e5327379093dc92ec2779d2c
bekhzod-olimov <bekhzod.olimov@gmail.com>
2024-07-04 09:37:11 +0900

    image retrieval

--- src/components/Project/Project.tsx
+++ src/components/Project/Project.tsx
@@ -129,6 +129,26 @@ export function Project() {
             <footer> <ul className="tech-list"> <li>PyTorch</li> <li>Image Generation</li> <li>GAN</li> <li>Imagen</li> </ul> </footer>
           </div>
         </ScrollAnimation>
+
+        <ScrollAnimation animateIn="flipInX">
+          <div className="project">
+            <header>
+              <svg width="50" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="#23ce6b" strokeWidth="1" strokeLinecap="round" strokeLinejoin="round"> <title>Folder</title> <path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path> </svg>
+              <div className="project-links">
+              <a href="https://github.com/bekhzod-olimov/Object-Classification-and-Classifier-Analysis" target="_blank" rel="noreferrer">
+                  <img src={githubIcon} alt="Visit site" /></a>
+                  <a href="https://github.com/vitasoftAI/ImageRetrievalResearch" target="_blank" rel="noreferrer">
+                  <img src={githubIcon} alt="Visit site" /></a>
+                  <a href="https://github.com/vitasoftAI/Recycle-Park" target="_blank" rel="noreferrer">
+                  <img src={githubIcon} alt="Visit site" /></a></div>
+            </header>
+            <div className="body">
+              <h3>Image Retrieval task-related research</h3>
+              <p> Conducted research on retrieving real-life images based on the sketch images. The trained model was implemented to detect fake luxury goods in the Customs office of the Republic of Korea and to detect used car parts in the GPARTS web-site.   </p>
+            </div>
+            <footer> <ul className="tech-list"> <li>PyTorch</li> <li>PyTorch Lightning</li> <li>Triplet training</li> <li>Imagen</li> </ul> </footer>
+          </div>
+        </ScrollAnimation>
       </div>
     </Container>
   );
