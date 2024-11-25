8dbe4584999edb2dd7da399a8582026692eb92a6
bekhzod-olimov <bekhzod.olimov@gmail.com>
2024-11-25 15:08:12 +0900

    add remove anything project

--- src/components/Project/Project.tsx
+++ src/components/Project/Project.tsx
@@ -289,6 +289,24 @@ export function Project() {
           </div>
         </ScrollAnimation>
 
+        <ScrollAnimation animateIn="flipInX">
+          <div className="project">
+            <header>
+              <svg width="50" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="#23ce6b" strokeWidth="1" strokeLinecap="round" strokeLinejoin="round"> <title>Folder</title> <path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path> </svg>
+              <div className="project-links">
+              <a href="https://github.com/bekhzod-olimov/Remove-Anything-with-Multiple-Points" target="_blank" rel="noreferrer">
+                  <img src={githubIcon} alt="Visit site" /></a>
+                </div>
+            </header>
+            <div className="body">
+              <h3>Remove Anything with Multiple Points</h3>
+              <p> This mini-project leverages the Segment Anything model for semantic segmentation to identify and isolate objects in images. It then uses the LaMA 2 inpainting model to seamlessly remove the selected objects. Unlike the original Remove Anything model, which does not support multiple segmentation points, this project addresses that limitation by enabling segmentation of multiple objects simultaneously, enhancing the flexibility and accuracy of object removal for complex tasks. </p>
+            </div>
+            <footer> <ul className="tech-list"> <li>PyTorch</li> <li>MultiModal Data</li> <li>Image Classification</li> <li>Sentiment Analysis</li> </ul> </footer>
+          </div>
+        </ScrollAnimation>
+
+
 
       </div>
     </Container>
