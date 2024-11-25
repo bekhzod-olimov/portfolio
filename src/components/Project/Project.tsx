d9830ad7aa166eb3eda7222fd306d92d61b03064
bekhzod-olimov <bekhzod.olimov@gmail.com>
2024-11-25 16:29:20 +0900

    add mil project

--- src/components/Project/Project.tsx
+++ src/components/Project/Project.tsx
@@ -306,6 +306,23 @@ export function Project() {
           </div>
         </ScrollAnimation>
 
+        <ScrollAnimation animateIn="flipInX">
+          <div className="project">
+            <header>
+              <svg width="50" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="#23ce6b" strokeWidth="1" strokeLinecap="round" strokeLinejoin="round"> <title>Folder</title> <path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path> </svg>
+              <div className="project-links">
+              <a href="https://github.com/bekhzod-olimov/Multi-Instance-Learning" target="_blank" rel="noreferrer">
+                  <img src={githubIcon} alt="Visit site" /></a>
+                </div>
+            </header>
+            <div className="body">
+              <h3> Multi Instance Learning </h3>
+              <p> This project implements a robust Multi-Instance Learning (MIL) framework designed to classify complex data where individual instances belong to different categories within a larger bag. The system dynamically learns bag-level labels from instance-level features, leveraging advanced AI techniques for both interpretability and performance. By optimizing attention-based mechanisms, the model identifies and weighs critical instances, making it highly effective for challenging tasks like image and text fusion in multimodal datasets. </p>
+            </div>
+            <footer> <ul className="tech-list"> <li> Semantic Segmentation </li> <li> Image Inpainting </li> <li> Object Removal </li> <li> PyTorch </li> </ul> </footer>
+          </div>
+        </ScrollAnimation>
+
 
 
       </div>
