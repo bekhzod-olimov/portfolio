76d854790a260c4d9452f2e7be2e444e2f9416d8
bekhzod-olimov <bekhzod.olimov@gmail.com>
2024-11-25 15:01:07 +0900

    add multimodal project

--- src/components/Project/Project.tsx
+++ src/components/Project/Project.tsx
@@ -272,6 +272,28 @@ export function Project() {
           </div>
         </ScrollAnimation>
 
+        <ScrollAnimation animateIn="flipInX">
+          <div className="project">
+            <header>
+              <svg width="50" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="#23ce6b" strokeWidth="1" strokeLinecap="round" strokeLinejoin="round"> <title>Folder</title> <path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path> </svg>
+              <div className="project-links">
+              <a href="https://drive.google.com/file/d/17gJVC79Lk8k4Uly5egCGynl7RFvKF3cZ/view?usp=sharing" target="_blank" rel="noreferrer">
+                  <img src={DownloadApp} alt="Visit site" /></a>
+                  <a href="https://github.com/bekhzod-olimov/Manga2WebtoonDevelopment" target="_blank" rel="noreferrer">
+                  <img src={githubIcon} alt="Visit site" /></a>
+                  <a href="https://drive.google.com/file/d/1ESbotxntqO2_hYlHtGdIZwI7LoZgYRGE/view?usp=sharing" target="_blank" rel="noreferrer">
+                  <img src={DownloadApp} alt="Visit site" /></a>
+                </div>
+            </header>
+            <div className="body">
+              <h3>Multimodal Medical Image Classification</h3>
+              <p> Dual input (of an image and text) allows for more accurate diagnosis and decision-making in healthcare, especially in scenarios where visual data alone may be ambiguous, leading to improved clinical insights and personalized treatments for patients.  </p>
+            </div>
+            <footer> <ul className="tech-list"> <li>PyTorch</li> <li>MultiModal Data</li> <li>Image Classification</li> <li>Sentiment Analysis</li> </ul> </footer>
+          </div>
+        </ScrollAnimation>
+
+
       </div>
     </Container>
   );
