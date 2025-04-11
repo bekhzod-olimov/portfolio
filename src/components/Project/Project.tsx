cbd5da51052ba5e3f486d32c6fdb242381b5476c
bekhzod-olimov <bekhzod.olimov@gmail.com>
2025-04-11 15:48:55 +0900

    add another project

--- src/components/Project/Project.tsx
+++ src/components/Project/Project.tsx
@@ -341,6 +341,27 @@ export function Project() {
           </div>
         </ScrollAnimation>
 
+        <ScrollAnimation animateIn="flipInX">
+          <div className="project">
+            <header>
+              <svg width="50" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="#23ce6b" strokeWidth="1" strokeLinecap="round" strokeLinejoin="round"> <title>Folder</title> <path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path> </svg>
+              <div className="project-links">
+              <a href="https://drive.google.com/file/d/1nHjvTZ4L-cYj-BLYnh8YT8cJ4BAtaLJu/view?usp=sharing" target="_blank" rel="noreferrer">
+                  <img src={DownloadApp} alt="Visit site" /></a>
+                  <a href="https://github.com/bekhzod-olimov/LicencePlateDetector" target="_blank" rel="noreferrer">
+                  <img src={githubIcon} alt="Visit site" /></a>
+                  <a href="https://drive.google.com/file/d/1nHjvTZ4L-cYj-BLYnh8YT8cJ4BAtaLJu/view?usp=sharing" target="_blank" rel="noreferrer">
+                  <img src={DownloadApp} alt="Visit site" /></a>
+                </div>
+            </header>
+            <div className="body">
+              <h3>Manga2Webtoon using AI </h3>
+              <p> Embarking on a project to convert Japanese and Chinese manga into Korean Webtoons using AI involves several sequential steps. These include segmenting parts using semantic segmentation, applying color to the original grayscale images using image generation models, extracting text from speech bubbles using Optical Character Recognition (OCR), and more.  </p>
+            </div>
+            <footer> <ul className="tech-list"> <li>PyTorch</li> <li>Semantic Segmentation</li> <li>OCR</li> <li>Object Detection</li> </ul> </footer>
+          </div>
+        </ScrollAnimation>
+
 
 
       </div>
