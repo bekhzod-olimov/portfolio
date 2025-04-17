f6f4387cef4800721fc45451556152d68850bf74
bekhzod-olimov <bekhzod.olimov@gmail.com>
2025-04-17 14:54:23 +0900

    add demo icon to the projects list

--- src/components/Project/Project.tsx
+++ src/components/Project/Project.tsx
@@ -3,7 +3,9 @@ import githubIcon from "../../assets/github.svg"
 import scholarIcon from "../../assets/google_scholar.svg"
 import kaggleIcon from "../../assets/kaggle.svg"
 import DownloadApp from '../../assets/play.svg'
+import DemoIcon from '../../assets/external-link.svg'
 import ScrollAnimation from "react-animate-on-scroll";
+
 
 export function Project() {
   return (
@@ -347,7 +349,7 @@ export function Project() {
               <svg width="50" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="#23ce6b" strokeWidth="1" strokeLinecap="round" strokeLinejoin="round"> <title>Folder</title> <path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path> </svg>
               <div className="project-links">
               <a href="https://licenceplatedetector-3lpdteifwtxr2j8lpdvdci.streamlit.app" target="_blank" rel="noreferrer">
-                  <img src={DownloadApp} alt="Visit site" /></a>
+                  <img src={DemoIcon} alt="Visit site" /></a>
                   <a href="https://github.com/bekhzod-olimov/LicencePlateDetector" target="_blank" rel="noreferrer">
                   <img src={githubIcon} alt="Visit site" /></a>
                   <a href="https://drive.google.com/file/d/1nHjvTZ4L-cYj-BLYnh8YT8cJ4BAtaLJu/view?usp=sharing" target="_blank" rel="noreferrer">
