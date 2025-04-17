50bc6a9df5975e2f6cd4618da1f047704f1d420c
bekhzod-olimov <bekhzod.olimov@gmail.com>
2025-04-17 14:51:13 +0900

    update licence plate detector project info

--- src/components/Project/Project.tsx
+++ src/components/Project/Project.tsx
@@ -346,7 +346,7 @@ export function Project() {
             <header>
               <svg width="50" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="#23ce6b" strokeWidth="1" strokeLinecap="round" strokeLinejoin="round"> <title>Folder</title> <path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path> </svg>
               <div className="project-links">
-              <a href="https://drive.google.com/file/d/1nHjvTZ4L-cYj-BLYnh8YT8cJ4BAtaLJu/view?usp=sharing" target="_blank" rel="noreferrer">
+              <a href="https://licenceplatedetector-3lpdteifwtxr2j8lpdvdci.streamlit.app" target="_blank" rel="noreferrer">
                   <img src={DownloadApp} alt="Visit site" /></a>
                   <a href="https://github.com/bekhzod-olimov/LicencePlateDetector" target="_blank" rel="noreferrer">
                   <img src={githubIcon} alt="Visit site" /></a>
@@ -358,11 +358,9 @@ export function Project() {
               <h3>Licence Plate Detector from Image and Video </h3>
               <p> Developed a License Plate Detector that accurately identifies license plates in both images and videos using a combination of object detection models and image processing techniques. The project includes real-time video frame analysis, bounding box visualization, and an interactive UI for uploading and testing various media formats.  </p>
             </div>
-            <footer> <ul className="tech-list"> <li>PyTorch</li> <li>Semantic Segmentation</li> <li>OCR</li> <li>Object Detection</li> </ul> </footer>
+            <footer> <ul className="tech-list"> <li>PyTorch</li> <li>Object Detection</li> <li>OCR</li> <li>YOLOv11</li> </ul> </footer>
           </div>
         </ScrollAnimation>
-
-
 
       </div>
     </Container>
