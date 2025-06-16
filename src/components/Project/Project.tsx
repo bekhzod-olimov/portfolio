0e3dcb62a46b81fc26f6ad86cc0bdb8865fe56b4
bekhzod-olimov <bekhzod.olimov@gmail.com>
2025-06-16 13:33:03 +0900

    update medical image classification and segmentation projects

--- src/components/Project/Project.tsx
+++ src/components/Project/Project.tsx
@@ -94,7 +94,7 @@ export function Project() {
             <header>
               <svg width="50" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="#23ce6b" strokeWidth="1" strokeLinecap="round" strokeLinejoin="round"> <title>Folder</title> <path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path> </svg>
               <div className="project-links">
-                  <a href="https://github.com/bekhzod-olimov/MedicalSemanticSegmentation3D" target="_blank" rel="noreferrer">
+                  <a href="https://github.com/bekhzod-olimov/BackgroundRemovalSemanticSegmentation" target="_blank" rel="noreferrer">
                   <img src={githubIcon} alt="Visit site" /></a></div>
             </header>
             <div className="body">
@@ -161,12 +161,16 @@ export function Project() {
             <header>
               <svg width="50" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="#23ce6b" strokeWidth="1" strokeLinecap="round" strokeLinejoin="round"> <title>Folder</title> <path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path> </svg>
               <div className="project-links">
-                  <a href="https://github.com/bekhzod-olimov/MedicalSemanticSegmentation3D" target="_blank" rel="noreferrer">
-                  <img src={githubIcon} alt="Visit site" /></a></div>
+              <a href="https://github.com/bekhzod-olimov/MedicalImageClassificationProjects" target="_blank" rel="noreferrer">
+              <img src={githubIcon} alt="Visit site" /></a>
+              <a href="https://medicalimageclassificationprojects-5wm8jkvqevzr6bfbwxdhnm.streamlit.app/" target="_blank" rel="noreferrer">
+              <img src={DownloadApp} alt="Visit site" /></a>
+              <a href="https://github.com/bekhzod-olimov/MedicalSemanticSegmentation3D" target="_blank" rel="noreferrer">
+              <img src={githubIcon} alt="Visit site" /></a></div>
             </header>
             <div className="body">
-              <h3>3D Medical Image Segmentation task-related research</h3>
-              <p> I conducted research on segmenting 3D medical image data (specifically, body organs, like spleen, prostate and so on along with dental nerve. The latter was a joint project with a South Korean dental clinic. The segmentation AI model is integrated to the South Korean dental clinic software. </p>
+              <h3>2D & 3D Medical Image Classification & Segmentation task-related research</h3>
+              <p> I conducted research on segmenting 2D Image classification and 3D medical image data (specifically, body organs, like spleen, prostate and so on along with dental nerve. The latter was a joint project with a South Korean dental clinic. The segmentation AI model is integrated to the South Korean dental clinic software. </p>
             </div>
             <footer> <ul className="tech-list"> <li>PyTorch</li> <li>3D medical data</li> <li>PyTorch Lightning</li> <li>Dentistry</li> </ul> </footer>
           </div>
