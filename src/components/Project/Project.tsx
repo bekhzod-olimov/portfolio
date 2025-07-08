2086a5e2f2b4116c4b5241d2fb0f31b89df2da3a
bekhzod-olimov <bekhzod.olimov@gmail.com>
2025-07-08 13:33:01 +0900

    update project information

--- src/components/Project/Project.tsx
+++ src/components/Project/Project.tsx
@@ -358,6 +358,27 @@ export function Project() {
           </div>
         </ScrollAnimation>
 
+        <ScrollAnimation animateIn="flipInX">
+          <div className="project">
+            <header>
+              <svg width="50" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="#23ce6b" strokeWidth="1" strokeLinecap="round" strokeLinejoin="round"> <title>Folder</title> <path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path> </svg>
+              {/* <div className="project-links"> */}
+              {/* <a href="https://licenceplatedetector-yophut2tkugx7rrmjfmmfd.streamlit.app/" target="_blank" rel="noreferrer">
+                  <img src={DemoIcon} alt="Visit site" /></a>
+                  <a href="https://github.com/bekhzod-olimov/LicencePlateDetector" target="_blank" rel="noreferrer">
+                  <img src={githubIcon} alt="Visit site" /></a>
+                  <a href="https://drive.google.com/file/d/1nHjvTZ4L-cYj-BLYnh8YT8cJ4BAtaLJu/view?usp=sharing" target="_blank" rel="noreferrer">
+                  <img src={DownloadApp} alt="Visit site" /></a>
+                </div> */}
+            </header>
+            <div className="body">
+              <h3>Image Generation Projects </h3>
+              <p> Coming soon...  </p>
+            </div>
+            <footer> <ul className="tech-list"> <li>PyTorch</li> <li>Image Generation</li> <li>Diffusion</li> <li>GAN</li> </ul> </footer>
+          </div>
+        </ScrollAnimation>
+
       </div>
     </Container>
   );
